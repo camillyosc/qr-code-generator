4c4d21fb68c73d197cd58eb8f59290ca696c1a00
Camilly Cristine <154565850+camillyosc@users.noreply.github.com>
2025-09-16 21:04:55 -0300

    Adicionando estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import {QRCodeCanvas} from 'qrcode.react';
+import { FaUpload } from "react-icons/fa";
+
 
 export default function Home() {
   return (
@@ -14,10 +19,118 @@ export default function Home() {
             <label htmlFor="link">
               Digite seu link:
             </label>
+
+            <input
+              type="text"
+              id="link"
+              placeholder="Seu link aqui"
+            />
           </div>
+
+          <div className='qr-code-preview'>
+            <p>
+              QR Code Preview
+            </p>
+
+            <QRCodeCanvas
+              value={"https://devemdev.com"}
+              title={"https://devemdev.com"}
+              size={200}
+              bgColor={"#1ec50fff"}
+              fgColor={"#ffffffff"}
+              level={"L"}
+              imageSettings={{
+                src: "https://static.zpao.com/favicon.png",
+                x: undefined,
+                y: undefined,
+                height: 44,
+                width: 40,
+                opacity: 1,
+                excavate: true,
+              }}
+            />
+
+          </div>
+
         </div>
 
         <div className="qr-code-customization">
+          <div className="customization-container">
+            <h3>Cores</h3>
+
+            <div className="input-container colors">
+              <div className="input-box">
+                <label htmlFor="fgColor">
+                  Cor principal
+                </label>
+                <input 
+                  type="color" 
+                  className='input-color'
+                  id='fgColor'
+                />
+              </div>
+
+                 <div className="input-box">
+                <label htmlFor="bgColor">
+                  Cor do fundo
+                </label>
+                <input 
+                  type="color" 
+                  className='input-color'
+                  id='bgColor'
+                />
+              </div>
+
+
+
+            </div>
+
+          </div>
+
+           <div className="customization-container">
+            <h3>Logo</h3>
+
+            <div className="input-container">
+              <div className="input-box">
+                <label htmlFor="logo">
+                  Insira seu logo
+                </label>
+                <input 
+                  type="file" 
+                  className='input-file'
+                  id='logo'
+                  accept='image/*'
+                />
+
+                <button className='input-file-button'>
+                  <FaUpload />
+                  Escolher arquivo 
+                </button>
+              </div>
+
+
+
+                 <div className="input-box">
+                <label htmlFor="logoSize">
+                  Tamanho da logo
+                </label>
+               <select name="logoSize" 
+                id="logoSize">
+
+                  <option value="24">24px x 38px</option>
+                  <option value="24">38px x 38px</option>
+                  <option value="50">50px x 50px</option>
+
+               </select>
+              </div>
+            </div>
+            
+          </div>
+
+          <button className='download-button'>
+            Baixar QR Code
+
+          </button>
 
         </div>
       </section>
